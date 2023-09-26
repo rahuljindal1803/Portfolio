0083b57c3ead8344b1aae05ebd42a81ce9609720
Rahul Jindal <rahuljindal1803@gmail.com>
2023-09-26 20:02:22 +0530

    second block done , js (working)

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ menu.onclick = () => {
   menu.classList.toggle("bx_menu");
   navbar.classList.toggle("open");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      console.log("clicked");
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
