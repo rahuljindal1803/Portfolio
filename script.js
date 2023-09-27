893a38e4506bbe87dc03c6f4a058b3a34b1b3e04
Rahul Jindal <rahuljindal1803@gmail.com>
2023-09-27 20:16:20 +0530

    Home page and Let's Connect btn done

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let navbar = document.querySelector(".nav__links");
 menu.onclick = () => {
   menu.classList.toggle("bx_menu");
   navbar.classList.toggle("open");
+  menu.classList.toggle("bx_menu");
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -13,16 +14,35 @@ document.addEventListener("DOMContentLoaded", function () {
     link.addEventListener("click", function (e) {
       e.preventDefault();
 
-      console.log("clicked");
-      const targetId = this.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
 
       if (targetElement) {
+        const offset = 90;
+        const targetPosition =
+          targetElement.getBoundingClientRect().top + window.scrollY - offset;
+        const easing = "cubic-bezier(0.25, 0.1, 0.25, 1)";
+
         window.scrollTo({
-          top: targetElement.offsetTop,
+          top: targetPosition,
           behavior: "smooth",
+          easing: easing,
         });
       }
     });
   });
+});
+
+// script.js
+const signupButton = document.getElementById("connect-btn");
+const closeBtn = document.getElementById("close-button");
+const signupFormContainer = document.getElementById("signup-form-container");
+
+signupButton.addEventListener("click", (e) => {
+  signupFormContainer.style.display = "flex";
+  e.preventDefault();
+});
+
+closeBtn.addEventListener("click", () => {
+  signupFormContainer.style.display = "none";
 });
